7d9bcf5970dfffc2b098243906ad2c2225068730
swatiroogi28 <swatiroogi_28@gmail.com>
2026-09-18 14:19:57 +0000

    done untill check image capacity

--- encode.c
+++ encode.c
@@ -93,6 +93,36 @@ Status read_and_validate_encode_args(char *argv[], EncodeInfo *encInfo)
         return e_failure
     return e_sucess
     */
+    int len=strlen(argv[2]);
+    if(len<4 || argv[2][len-4]!='.'|| argv[2][len-3]!='b'|| argv[2][len-2]!='m'|| argv[2][len-1]!='p')
+    {
+        printf("Error: source file should be .bmp\n");
+        return e_failure;
+    }
+    encInfo->src_image_fname=argv[2];
+    encInfo->secret_fname=argv[3];
+    if(argv[4]==NULL)
+    {
+        encInfo->stego_image_fname="output.bmp";
+    }
+    else
+    {
+        len=strlen(argv[4]);
+        if(len<4 ||argv[4][len-4]!='.'|| argv[4][len-3]!='b'|| argv[4][len-2]!='m'|| argv[4][len-1]!='p')
+        {
+            printf("Error: output file should be .bmp\n");
+            return e_failure;
+        }
+        encInfo->stego_image_fname=argv[4];
+    }
+    if(open_file(encInfo)==e_failure)
+    {
+        printf("Error: unable to open file\n");
+        return e_failure;
+    }
+    return e_success;
+
+
 }
 Status open_files(EncodeInfo *encInfo)
 {
@@ -110,9 +140,29 @@ Status open_files(EncodeInfo *encInfo)
         fptr_stego_image=fopen()
     return e_success
     */
+    encInfo->fptr_src_image=fopen(encInfo->src_image_fname,"r");
+    if(encInfo->fptr_src_image==NULL)
+    {
+        printf("Failed to open Source file\n");
+        return e_failure;
+    }
+    encInfo->fptr_secret=fopen(encInfo->secret_fname,"r");
+    if(encInfo->fptr_secret==NULL)
+    {
+        printf("Failed to open secret file\n");
+        return e_failure;
+    }
+    encInfo->fptr_stego_image=fopen(encInfo->stego_image_fname,"w");
+    if(encInfo->fptr_stego_image==NULL)
+    {
+        printf("Failed to open output file\n");
+        return e_failure;
+    }
+    return e_success;
 }
 Status do_encoding(EncodeInfo *encInfo)
 {
+
     /*
     // call check_capacity(encInfo)==e_failure
         print error msg ,return e_failure
@@ -120,9 +170,24 @@ Status do_encoding(EncodeInfo *encInfo)
         print error msg,return e_failure
     // call encode_magic_string(MAGIC_STRING,encInfo)==e_failure
         print error msg ,return e_failure
-    // call 
-
+    // call encode_secret_file_extn_size(encInfo)==e_failure
+         print error msg ,return e_failure
+    // call encode_secret_file_extn_size(extern_secret_file,encInfo)==e_failure
+         print error msg ,return e_failure
+    // call encode_secret_file_size(size_secret_file,encInfo)==e_failure
+        print error msg ,return e_failure
+    // call encode_secret_file_data(encInfo)== e_failure
+        print error msg ,return e_failure
+    // call copy_remaining_img_data(fptr_scr_file,fptr_dest_file)== e_failure
+        print error msg ,return e_failure
+        
+    return e_success
     */
+    if(check_capacity(encInfo)==e_failure)
+    {
+        printf("Error: check capacity failed\n");
+        return e_failure;
+    }
 }
 Status check_capacity(EncodeInfo *encInfo)
 {
@@ -135,6 +200,15 @@ Status check_capacity(EncodeInfo *encInfo)
         return e_failure
     return e_sucess
     */
+    encInfo->image_capacity=get_image_size_for_bmp(encInfo->fptr_src_image);
+    encInfo->size_secret_file=get_file_size(encInfo->fptr_secret);
+    if(((14+size_secret_file)*8) > encInfo->image_capacity)
+    {
+        printf("Insufficient Image capacity\n");
+        return e_failure;
+    }
+    return e_success;
+    
 }
 uint get_file_size(FILE *fptr)
 {
@@ -187,8 +261,68 @@ Status encode_secret_file_extn_size( EncodeInfo *encInfo)
         -> declare a buff[32]
         -> read 32 byte from scr_file into buff
         -> call encode_size_to_lsb(strlen(extern_secret_file),buff)
+        -> write the 32 bytes buffer to output_file
+        return e_success;
     */
 }
 
 
-Status encode_size_to_lsb()
+Status encode_size_to_lsb(int size,char *Image_buff)
+{
+    /*
+        for(int i=31;i>=0;i--)
+        {
+         ->get the ith bit is set or not
+            ==if set,set the lsb of image_buffer[not i make expression]
+            == if clear,clear the lsb of image_buffer[]
+
+    // find the error part and return e_failure
+        }
+        return e_success;
+    */
+}
+Status encode_secret_file_extn(const char *file_extn, EncodeInfo *encInfo)
+{
+    /*
+        declare buff[8]
+        -> read 8 byte from scr_image
+        ->encode_byte_to_lsb(file_extn[],buff)
+        -> write the 8 bytes of buff to out;put_file
+
+        return e_success;
+
+    */
+}
+Status encode_secret_file_size(long file_size, EncodeInfo *encInfo)
+{
+    /*
+        -> declare buff[32]
+
+        -> read 32 byte from scr_image
+        -> call encode_size_to_lsb(file_fice,buff)
+        -> write the 32 bytes of buff to output_file
+        return e_success
+    */
+}
+Status encode_secret_file_data(EncodeInfo *encInfo)
+{
+    /*
+        declare the buffer[8],data
+    => loop  for  extension length till eof of secret_file
+        -> read 8 bytes from scr_file
+        -> read one byte from secret_file(char by char)
+        -> call encode_byte_to_lsb(data,buff)
+    return e_success
+    */
+}
+Status copy_remaining_img_data(FILE *fptr_src, FILE *fptr_dest)
+{
+    /*
+        -> declare a char as a data
+    => loop till eof of scr_file
+        -> read a char from source file 
+        -> write the data to dest_file
+
+    return e_success
+    */
+}
--- test_encode.c
+++ test_encode.c
@@ -5,15 +5,38 @@
 int main(int argc,char *argv[])
 {
     EncodeInfo encInfo;
-    /* if(check_operation_type(argv[1][1])==e_encode)
+    /*
+    if(check_operation_type(argv[1][1])==e_encode)
     -> call read_and_validate_encode_args(argv,&encInfo)==e_sucess
-        ->Status do_encoding(EncodeInfo *encInfo);
+        ->call do_encoding(&encInfo)==e_success
+            print "Encoding is success"
 
     */
+    if(check_operation_type(argv[1][1])==e_encode)
+    {
+        if(read_and_validate_encode_args(argv,&encoInfo)==e_success)
+        {
+            if(do_encoding(encoInfo)==e_success)
+            {
+                printf("Encoding is success\n");
+            }
+        }
+    }
     return 0;
 }
 OperationType check_operation_type(char opt)
 {
-
+    if(opt=='e')
+    {
+        return e_encode;
+    }
+    else if(opt=='d')
+    {
+        return e_decode;
+    }
+    else
+    {
+        return e_unsupported;
+    }
 
 }
